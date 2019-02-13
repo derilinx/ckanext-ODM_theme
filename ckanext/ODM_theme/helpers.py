d51c49c8345cf866572f351f0e062c6a48f547e7
Eric Soroos <eric@derilinx.net>
2019-02-13 16:47:44 +0000

    removed absolute path, fixed helper import, removed builtin shadowing

--- ckanext/ODM_theme/helpers.py
+++ ckanext/ODM_theme/helpers.py
@@ -1,17 +1,18 @@
 import json
+from ckan.lib.helpers import helper_functions
+
 import logging
-from ckanext.odm_nav.lib import odm_nav_helper as hlp
 log = logging.getLogger(__name__)
 
+import os
 
 def get_odm_navigation():
 
-    filename = '/usr/lib/ckan-extensions/ckanext-ODM_theme/ckanext/ODM_theme/public/odm_nav_items.json'
-    with open(filename) as file:
-        menu_items = json.load(file)
-    file.close()
+    filename = os.path.join(os.path.dirname(__file__), 'public', 'odm_nav_items.json')
+    with open(filename) as f:
+        menu_items = json.load(f)
 
-    return hlp.nav_html_parsing(menu_items)
+    return helper_functions.get('nav_html_parsing')(menu_items)
 
 
 
